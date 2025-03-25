39a9298daf61f3a9271518be97b3c34edc27026c
Andrew Tang <disandrewed@gmail.com>
2025-03-25 12:52:25 +1100

    updated url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // Service for handling API calls
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api/exchange';
+const API_BASE_URL = 'teenage-karlotta-disandrewed-9ca204c3.koyeb.app/api/exchange';
 
 // Fetch all orderbooks
 export const fetchAllOrderbooks = async () => {
